c267e1d566a4524bd03e6edaeb3b0ae12cdae52c
ecsumed <>
2015-12-01 15:43:10 +0500

    added addEvent call.

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import json
+
 import peewee as pw
 
 import local
@@ -11,6 +13,25 @@ db = pw.MySQLDatabase(local.DATABASE,
                       passwd=local.PASSWD)
 
 
+class Point(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def toJson(self, ):
+        return json.dumps({'x': self.x,
+                           'y': self.y
+                           })
+
+    def fromJson(self, json_pt):
+        dict_pt = json.loads(json_pt)
+        self.x = dict_pt['x']
+        self.y = dict_pt['y']
+
+        return self
+
+
 class MySQLModel(pw.Model):
     """A base model that will use our MySQL database"""
     class Meta:
@@ -21,6 +42,21 @@ class Event_Type(MySQLModel):
     id = pw.IntegerField(primary_key=True)
     name = pw.CharField()
 
+    @classmethod
+    def get_id(cls, event_type_name):
+        """
+        Returns id of event type specified.
+        """
+        try:
+            type_ = cls.get(cls.name == event_type_name)
+        except pw.DoesNotExist:
+            type_ = None
+
+        if type_:
+            return type_.id
+        else:
+            return None
+
 
 class Event(MySQLModel):
     id = pw.IntegerField(primary_key=True)
@@ -29,7 +65,19 @@ class Event(MySQLModel):
     report_time = pw.DateTimeField(default=datetime.now())
     event_type = pw.ForeignKeyField(Event_Type, related_name='id')
 
+    @classmethod
+    def addEvent(cls, coord, event_time, report_time, event_type_id):
+        """
+        Creates a new event.
+        """
 
+        event = Event(coord=coord.toJson(),
+                      event_time=event_time,
+                      report_time=report_time,
+                      event_type_id=event_type_id)
+
+        event.save()
+        return (True, event)
 
 
 def create_tables(db):
