09305c856eb9dc51c272fae49b3e06ff76fba737
ecsumed <>
2015-12-10 18:16:40 +0500

    image with filled in areas.

--- google_api.py
+++ google_api.py
@@ -15,7 +15,17 @@ google_api_key = cycle(local.GOOGLE_API_KEYS)
 class Static_Map_API(object):
 
     def __init__(self, ):
-        pass
+        self.image_size = (settings.IMAGE_SIZE[0], settings.IMAGE_SIZE[1])
+        self.zoom_level = settings.ZOOM_LEVEL
+        self.scale = settings.SCALE
+        self.format_ = settings.FORMAT
+
+        self.line_weight = settings.LINE_WEIGHT
+        self.line_color = settings.LINE_COLOR
+
+        self.area_weight = settings.AREA_WEIGHT
+        self.area_color = settings.AREA_COLOR
+        self.area_fill_color = settings.AREA_FILL_COLOR
 
     def build_url(self, url_data):
         """
@@ -33,17 +43,17 @@ class Static_Map_API(object):
         """
 
         # add image size
-        params = 'size={}x{}&'.format(settings.IMAGE_SIZE[0],
-                                      settings.IMAGE_SIZE[1],
+        params = 'size={}x{}&'.format(self.image_size[0],
+                                      self.image_size[1],
                                       )
         # add zoom level
-        params += 'zoom={}&'.format(settings.ZOOM_LEVEL)
+        params += 'zoom={}&'.format(self.zoom_level)
 
         # add scale
-        params += 'scale={}&'.format(settings.SCALE)
+        params += 'scale={}&'.format(self.scale)
 
         # add image_type
-        params += 'format={}&'.format(settings.FORMAT)
+        params += 'format={}&'.format(self.format_)
 
         for event_type, coords in url_data.iteritems():
             event_info = Event_Type.\
@@ -52,12 +62,18 @@ class Static_Map_API(object):
             if not event_info.isSinglePoint:
                 if event_info.name == 'Rally':
                     for e in coords:
-                        params += 'path=color:{}|weight:{}'.format('0xff0000ff',
-                                                                   '5',
-                                                                   )
-                        for coord in e:
-                            params += '|{x},{y}'.format(x=coord.x, y=coord.y)
-                        params += '&'
+                        params += 'path=color:{}|weight:{}'.\
+                            format(self.line_color, self.line_weight)
+                        params += self.add_coordinates(e, True)
+
+                if event_info.name == 'Protest':
+                    for e in coords:
+                        params += 'path=color:{}|weight:{}|fillcolor:{}'.\
+                            format(self.area_color,
+                                   self.area_weight,
+                                   self.area_fill_color,
+                                   )
+                        params += self.add_coordinates(e, True)
             else:
                 if event_info.icon_url:
                     params += 'markers=icon:{}'.format(event_info.icon_url)
@@ -65,18 +81,33 @@ class Static_Map_API(object):
                     params += 'markers=color:{}|label:{}'.format('red',
                                                                  event_type[0],
                                                                  )
-                for coord in coords:
-                    params += '|{x},{y}'.format(x=coord.x, y=coord.y)
-
+                params += self.add_coordinates(coords, False)
             params += '&'
 
         url = settings.GOOGLE_BASE_URL.format(parameters=params,
                                               api_key=next(google_api_key),
                                               )
         # handle 2000 character limit url
-        print len(url)
-        print url
+        # print len(url)
         return url
+
+    def add_coordinates(self, coordinates, addAnd):
+        """
+            Adds a list of coordinates to a string to be appended to the static
+            image url. Also, appends an '&' if addAnd is True.
+            INPUT:
+                [{'x': <number>, 'y': <number>}], addAnd
+            OUTPUT:
+                String, ex. '27.456456,58.65248|54.5246,58.695223&'
+        """
+        coord_params = ''
+        for coord in coordinates:
+            coord_params += '|{x},{y}'.format(x=coord.x, y=coord.y)
+
+        if addAnd:
+            coord_params += '&'
+
+        return coord_params
 
     def fetch_image(self, url):
         """
